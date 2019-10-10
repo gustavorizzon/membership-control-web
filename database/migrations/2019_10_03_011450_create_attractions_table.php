87e1425e43ae7951b1cfdea6536d284fa99e6337
Gustavo Rizzon <gustavop.rizzon@gmail.com>
2019-10-09 22:06:00 -0300

    Attraction

---
+++ database/migrations/2019_10_03_011450_create_attractions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttractionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attractions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('name')->unique();
+            $table->text('description');
+
+            // Attraction Type Reference
+            $table->integer('attraction_type_id');
+            $table->foreign('attraction_type_id')->references('id')->on('attractions_types');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attractions');
+    }
+}
