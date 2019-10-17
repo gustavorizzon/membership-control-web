8ecfbd86b753988c1a14dde866e8eddfd31bead1
Gustavo Rizzon <gustavop.rizzon@gmail.com>
2019-10-17 11:48:05 -0300

    Event Tickets

---
+++ database/migrations/2019_10_10_011226_create_event_tickets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_tickets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            // Ticket Type Reference
+            $table->integer('ticket_type_id');
+            $table->foreign('ticket_type_id')->references('id')->on('ticket_types');
+
+            // Event Reference
+            $table->bigInteger('event_id');
+            $table->foreign('event_id')->references('id')->on('events');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_tickets');
+    }
+}
