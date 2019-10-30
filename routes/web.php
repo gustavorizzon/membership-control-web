74c4e20d3c9c73f11de7bb428aa48c1c6caceee6
Gustavo Rizzon <gustavop.rizzon@gmail.com>
2019-10-30 17:09:30 -0300

    Installed AdminLTE and compiled a clean css/js preset.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
